d01f7e8ce6ec9002f59c80b4f294069b25100d71
Timofe <wrspace@gmail.com>
2026-04-17 09:35:00 +0500

    deploy commit folders

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Добавь эту строку
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // И эту строку
+  ],
 })
