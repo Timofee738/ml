04c2c1339c6cbd29b142259f05de46bff4d37565
Timofe <wrspace@gmail.com>
2026-04-19 17:45:25 +0500

    CORS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Добавь эту строку
+import tailwindcss from '@tailwindcss/vite' 
 
 export default defineConfig({
   plugins: [
